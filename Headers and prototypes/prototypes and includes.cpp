2717b5de00531fc709f55039bb5454a8eaca309e
nljor <nljor@JONSSONS>
2017-03-02 17:12:21 +0100

    Add project files.

---
+++ Headers and prototypes/prototypes and includes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "utils.h"
+
+
+using namespace std;
+
+
+int main() {
+	doSomething();
+	doSomething();
+
+	return 0;
+}
+
+
+// Implementation
+void doSomething() {
+	cout << "Hello! " << endl;
+
+}
---
+++ Headers and prototypes/utils.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Prototypes, shows what it returns and what it accepts
+void doSomething();
